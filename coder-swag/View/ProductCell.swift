460d3f66a018949c802412bb4b5251e12ddf6bec
tsoo6464 <tsoo6464@gmail.com>
2018-01-12 00:14:22 +0800

    Collection Views(grid layouts)

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Nan on 2018/1/12.
+//  Copyright © 2018年 nan. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
